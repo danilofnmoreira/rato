eb71d2759d42364291907f5bbd8f6fd18e5efb88
Danilo Moreira <danilofnmoreira@gmail.com>
2021-02-08 23:57:39 -0300

    app estruturado

---
+++ app/src/main/java/com/danilove/rato/Constants.java
@@ -0,0 +1,6 @@
+package com.danilove.rato;
+
+public class Constants {
+
+    public static final int ERROR_DIALOG_REQUEST = 9001;
+}
---
+++ app/src/main/java/com/danilove/rato/ui/IProfile.java
@@ -0,0 +1,6 @@
+package com.danilove.rato.ui;
+
+public interface IProfile {
+
+    void onImageSelected(int resource);
+}
